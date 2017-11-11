422d837dc53ed3e62dc08fd5083977f65e95dde1
5730593821-SP <supakritboom@hotmail.com>
2017-11-11 23:03:36 +0700

    Init Project With Atomic Boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import { injectGlobal, ThemeProvider } from 'styled-components'
+
+import { HomePage, SamplePage, NotFoundPage, TestPage } from 'components'
+
+// https://github.com/diegohaz/arc/wiki/Styling
+import theme from './themes/default'
+
+injectGlobal`
+  body {
+    margin: 0;
+  }
+`
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/sample-page" component={SamplePage} />
+        <Route path="/test-page" component={TestPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </ThemeProvider>
+  )
+}
+
+export default App

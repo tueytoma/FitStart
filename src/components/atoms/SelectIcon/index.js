d1ecf55d9a81af38aba311aea76597004343f712
Sorachach Panomchaisawang <sorachach-ko@hotmail.com>
2017-11-15 02:07:04 +0700

    Add SelectIcon, CloseIcon, Logo

---
+++ src/components/atoms/SelectIcon/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default class SelectIcon extends React.Component {
+
+  render() {
+    return (
+        <svg width="81" height="81" viewBox="0 0 81 81" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+			<g id="Canvas" transform="translate(828 213)">
+				<g id="select">
+					<g id="&#224;&#184;&#129;&#224;&#184;&#178;&#224;&#184;&#163;&#224;&#184;&#170;&#224;&#184;&#161;&#224;&#184;&#177;&#224;&#184;&#132;&#224;&#184;&#163;" filter="url(#filter0_d)">
+						<g id="&#224;&#184;&#155;&#224;&#184;&#163;&#224;&#184;&#176;&#224;&#185;&#128;&#224;&#184;&#160;&#224;&#184;&#151;">
+							<g id="Vector">
+								<use xlink:href="#path0_fill" transform="translate(-818 -203)" fill="#F05939"/>
+							</g>
+							<g id="Group">
+								<g id="Group">
+									<g id="Vector">
+										<use xlink:href="#path1_fill" transform="translate(-801.7 -183.5)" fill="#F7F8F8"/>
+									</g>
+								</g>
+							</g>
+						</g>
+					</g>
+				</g>
+			</g>
+			<defs>
+				<filter id="filter0_d" filterUnits="userSpaceOnUse" x="-828" y="-213" width="81" height="81" color-interpolation-filters="sRGB">
+					<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+					<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 255 0"/>
+					<feOffset dx="0" dy="0"/>
+					<feGaussianBlur stdDeviation="5"/>
+					<feColorMatrix type="matrix" values="0 0 0 0 0.945098 0 0 0 0 0.34902 0 0 0 0 0.227451 0 0 0 1 0"/>
+					<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+					<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+				</filter>
+				<path id="path0_fill" d="M 30.2 60.4C 46.879 60.4 60.4 46.879 60.4 30.2C 60.4 13.521 46.879 0 30.2 0C 13.521 0 0 13.521 0 30.2C 0 46.879 13.521 60.4 30.2 60.4Z"/>
+				<path id="path1_fill" d="M 27.4 3L 24.9 0.5C 24.6 0.2 24.2 3.8147e-07 23.7 3.8147e-07C 23.2 3.8147e-07 22.8 0.2 22.5 0.5L 10.7 12.4L 5.4 7.1C 5.1 6.8 4.7 6.6 4.2 6.6C 3.7 6.6 3.3 6.8 3 7.1L 0.5 9.6C 0.2 9.9 -3.8147e-07 10.3 -3.8147e-07 10.8C -3.8147e-07 11.3 0.2 11.7 0.5 12L 7 18.5L 9.5 21C 9.8 21.3 10.2 21.5 10.7 21.5C 11.2 21.5 11.6 21.3 11.9 21L 14.4 18.5L 27.5 5.4C 27.8 5.1 28 4.7 28 4.2C 28 3.7 27.7 3.3 27.4 3Z"/>
+			</defs>
+		</svg>
+    )
+  }
+
+}

f51f1c31221585e4a57e5467c680f2af2a20c90e
5730625221-SS <tueytobi@gmail.com>
2017-11-15 06:18:27 +0700

    fix Button

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -5,12 +5,13 @@ import { font } from 'styled-theme'
 import { ifProp } from 'styled-tools'
 
 const Button = styled.button`
+    padding: auto 30px;
     border: ${props => props.trainee ? "1px solid #F05939" : props.trainer ? "1px solid #211F5E" : "1px solid #F05939"};
     border-radius: 100px;
 
-    background-color: #F9FAFC;
-    height: ${props => props.cancleRegis ? "40px" : props.loginPage ? "29px" : props.width};
-    width: ${props => props.cancleRegis ? "175px" : props.loginPage ? "95px" : props.height};
+    background-color: #F9FAFC; 
+    height: ${props => props.cancleRegis ? "40px" : props.loginPage ? "29px" : props.height};
+    width: ${props => props.cancleRegis ? "175px" : props.loginPage ? "95px" : props.width};
     margin: 0 8px 0 0;
     
 
@@ -22,6 +23,7 @@ const Button = styled.button`
     color: ${props => props.trainee ? "#F05939" : props.trainer ? "#211F5E" : "#F05939"};
 
     outline: none;
+    cursor: pointer;
 
     &:hover {
         color: #F4F4F4;

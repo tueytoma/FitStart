8d2dc39c84c23c5458a8277495ced3e8d4d91818
5730593821-SP <supakritboom@hotmail.com>
2017-11-15 09:17:06 +0700

    Fix Warnings

--- src/components/atoms/CloseIcon/index.js
+++ src/components/atoms/CloseIcon/index.js
@@ -8,12 +8,12 @@ export default class CloseIcon extends React.Component {
 			<g id="Canvas" transform="translate(366 286)">
 				<g id="close">
 					<g id="button bg">
-						<path transform="translate(-366 -286)" fill="#C4C4C4" fill-opacity="0.8" fill-rule="evenodd" d="M 30.5 61C 
+						<path transform="translate(-366 -286)" fill="#C4C4C4" fillOpacity="0.8" fillRule="evenodd" d="M 30.5 61C 
 						47.3447 61 61 47.3447 61 30.5C 61 13.6553 47.3447 0 30.5 0C 13.6553 0 0 13.6553 0 30.5C 0 47.3447 13.6553 61 30.5 61Z"/>
 					</g>
 					<g id="add">
 						<g id="Shape">
-							<path transform="matrix(0.707107 0.707107 -0.707107 0.707107 -335 -283)" fill="#FFFFFF" fill-rule="evenodd" 
+							<path transform="matrix(0.707107 0.707107 -0.707107 0.707107 -335 -283)" fill="#FFFFFF" fillRule="evenodd" 
 							d="M 39.598 16.9706L 22.6274 16.9706L 22.6274 0L 16.9706 0L 16.9706 16.9706L 0 16.9706L 0 22.6274L 16.9706 
 							22.6274L 16.9706 39.598L 22.6274 39.598L 22.6274 22.6274L 39.598 22.6274L 39.598 16.9706Z"/>
 						</g>

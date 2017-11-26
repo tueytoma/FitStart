881e034933f9140770cbd57fd77c055a749b3452
5730625221-SS <tueytobi@gmail.com>
2017-11-26 23:05:19 +0700

    fix bug search home

--- src/components/pages/HomePage/index.js
+++ src/components/pages/HomePage/index.js
@@ -111,6 +111,10 @@ class HomePage extends React.Component {
     location.reload();
   }
 
+  Search = e => {
+    location.reload();
+}
+
   render() {
     let color = auth.isLoggedIn() ? auth.isTrainer() ? "#211F5E" : auth.isTrainee() ? "#F05939" : "" : "#202020";
     var resultFeed = []
@@ -129,7 +133,7 @@ class HomePage extends React.Component {
               {!auth.isLoggedIn() && <Label size="32px" weight="normal" color="#202020">นี่คือระบบศูนย์รวมเทรนเนอร์ที่มีประสิทธิภาพมากมาย</Label>}
               <SearchBox>
                 <Textfield onChange={this.changeSearch} placeholder="อยากฝึกฝนร่างกายเกี่ยวกับ... / อยากฝึกกับ..." width="370px" height="30px" color={color}/>
-                <Link to= {'/search/service?keyword=' + this.state.search} style={{textDecoration: "none"}}>
+                <Link onClick={this.Search} to= {'/search/service?keyword=' + this.state.search} style={{textDecoration: "none"}}>
                     <SearchIcon opacity="0.5" color="#545454"/>
                 </Link>
                   

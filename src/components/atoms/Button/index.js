f6b6a4cf9fa6920d9df9bfbcef5a7502afdeae5e
5730625221-SS <tueytobi@gmail.com>
2017-11-26 22:07:14 +0700

    update Edit Profile page

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -7,10 +7,10 @@ import { ifProp } from 'styled-tools'
 const Button = styled.button`
 
     padding: auto 30px;
-    border: 2px solid ${props => props.disabled ? "#c4c4c4" : props.color}; 
+    border: 2px solid ${props => props.disabled ? "#c4c4c4" : props.dark ? "#F9FAFC" : props.color}; 
     border-radius: 100px;
 
-    background-color: #F9FAFC;
+    background-color: ${props => props.dark ? props.color : "#F9FAFC"};
     height: ${props => props.cancelregis ? "40px" : props.loginpage ? "40px" : props.height};
     width: ${props => props.cancelregis ? "175px" : props.loginpage ? "125px" : props.width};
     margin: 0 8px 0 0;
@@ -21,14 +21,14 @@ const Button = styled.button`
     font-weight: 800;
     line-height: normal;
     font-size: ${props => props.cancelregis ? "18px" : props.loginpage ? "16px" : props.size};
-    color: ${props => props.disabled ? "#c4c4c4" : props.color}; 
+    color: ${props => props.disabled ? "#c4c4c4" : props.dark ? "#F9FAFC" : props.color}; 
 
     outline: none;
     cursor: pointer;
 
     &:hover {
-        color: ${props => props.disabled ? "#c4c4c4" : "#F9FAFC"}; 
-        background-color: ${props => props.disabled ? "#F9FAFC" : props.color}; 
+        color: ${props => props.disabled ? "#c4c4c4" : props.dark ? props.color : "#F9FAFC"}; 
+        background-color: ${props => props.disabled ? "#F9FAFC" : props.dark ? "#F9FAFC" : props.color}; 
         cursor: ${props => props.disabled ? "default" : "pointer"};
     }
 `

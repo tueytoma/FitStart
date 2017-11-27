5445b71d0e94ea11e6563bfb4656e6a7151f71f6
5730593821-SP <supakritboom@hotmail.com>
2017-11-28 05:57:12 +0700

    Update Bath Icon Click

--- src/components/pages/SelectServicePage/index.js
+++ src/components/pages/SelectServicePage/index.js
@@ -173,7 +173,7 @@ class SelectServicePage extends React.Component {
   }
 
   componentDidMount() {
-    api.getReservationByStatus(this.state.status)
+    api.getReservationById(this.state.serviceID)
     .then((res)=>{
       this.setState({results : res})
     })
